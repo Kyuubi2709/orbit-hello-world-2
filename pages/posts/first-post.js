6cf67aa9854b15ac8272deaca9f93fa376df06b5
Kyuubi2709 <cryptokyuubi@protonmail.com>
2026-01-16 12:26:53 +0100

    Create first-post.js

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+export default function FirstPost() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>My First Post</h1>
+      <p>This is a sample blog post in a minimal Next.js app deployed on Orbit.</p>
+
+      <a href="/" style={{ color: "#0070f3" }}>
+        ← Back to home
+      </a>
+    </main>
+  );
+}
